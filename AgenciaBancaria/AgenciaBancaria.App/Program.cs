36910dd9085a48b96a33f627d64e311ad6da6b80
Chrystiomar <chrystiomar@hotmail.com>
2021-09-11 08:46:42 -0300

    criacao de classes e finalizacao

---
+++ AgenciaBancaria/AgenciaBancaria.App/Program.cs
@@ -0,0 +1,33 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("rua um", "5555666", "Curitiba", "PR");
+                Cliente cliente = new Cliente("Chrys", "00011122233", "99999987", endereco);
+                //ContaBancaria conta = new ContaBancaria(cliente);
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta " + conta.Situacao +": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                string senha = ("abcd1234");
+                conta.Abrir(senha);
+                Console.WriteLine("Conta " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+                Console.WriteLine("Saldo: " + conta.Saldo);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
